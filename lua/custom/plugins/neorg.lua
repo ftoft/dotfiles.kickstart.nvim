806bdf4233716ccd6876e832cb51d8da7db3e70e
Finn Skødt Toft <ftoft@fito.dk>
2025-10-25 20:40:06 +0200

    Added some plugins for Neorg, dirman, itero and promo. I also change some values for how the conceal works.

--- lua/custom/plugins/neorg.lua
+++ lua/custom/plugins/neorg.lua
@@ -11,6 +11,20 @@ return {
           icon_preset = 'varied',
         },
       },
+      ['core.dirman'] = {
+        config = {
+          workspaces = {
+            my_notes = '~/notes', -- Format: <name_of_workspace> = <path_to_workspace_root>
+          },
+          index = 'index.norg', -- The name of the main (root) .norg file
+        },
+      },
+      ['core.itero'] = {},
+      ['core.promo'] = {},
     },
   },
+  init = function()
+    vim.opt.conceallevel = 2
+    vim.opt.concealcursor = 'nc'
+  end,
 }
